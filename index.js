b5cfb0e308388ac3d52bd27ac4d96735c2b2b7c9
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-05-24 00:56:52 +0200

    CPU usage and some others

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 "use strict";
 
+const os = require("os");
 const doLongWork = require("./worker");
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send(`Hello World! ${new Date()}`))
+app.get("/", (req, res) => res.send(`Hello World! ${new Date()}`));
 
-app.get('/start/:proc/:size', (req, res) => { 
+app.get("/start/:proc/:size", (req, res) => {
     let num_processes = Number(req.params.proc);
-    let size = Number(req.params.size / num_processes);
-    start(num_processes, size); 
-    res.send(`ok\n`)
-})
+    start(num_processes, req.params.size);
+    res.send(`ok\n`);
+});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => {
+    console.log(`Try to open page http://localhost:${port}/start/2/10000000000`);
+    console.log(`Read README.md for more info`);
+    console.log();
+});
+
+console.log(`Number of logical CPUs: ${os.cpus().length}`);
+console.log(`CPU Speed: ${os.cpus()[0].speed}MHz`);
+console.log();
 
 let startTime = 0;
 
@@ -32,23 +40,63 @@ function getSecondsFromStart() {
     return now - startTime;
 }
 
-async function start(num_processes, size) {
-    let i = setInterval(progressBar, 250);
-    startTimer();
+async function start(num_processes, totalSize) {
+    console.log(
+        `Arrived request to count up to ${Number(totalSize).toLocaleString()} with ${num_processes} ${
+            num_processes === 1 ? "thread" : "threads"
+        }`,
+    );
+
+    let size = Number(totalSize / num_processes);
+    let intervalProgressBar = setInterval(progressBar, 250);
+    let startCpuUsage = os.cpus();
+
     let longWorks = [];
-    
+
+    startTimer();
+
     for (let count = 0; count < num_processes; count++) {
-        longWorks.push( doLongWork(size) );
+        longWorks.push(doLongWork(size));
     }
-    
+
     let responses = await Promise.all(longWorks);
+
+    const usedTime = getSecondsFromStart();
+
+    let endCpuUsage = os.cpus();
+
+    clearInterval(intervalProgressBar);
 
     let countUpTo = 0;
 
     for (const response of responses) {
         countUpTo += response;
     }
-    
-    console.log(`\nCounted up to ${countUpTo.toLocaleString()} with ${num_processes.toLocaleString()} ${num_processes===1?"thread":"threads"} in ${getSecondsFromStart().toLocaleString()} milliseconds`);
-    clearInterval(i);
-} 
+
+    let CPUsUsage = [];
+
+    for (let cpu = 0; cpu < os.cpus().length; cpu++) {
+        const cpuUsage = {
+            index: cpu,
+            user: (endCpuUsage[cpu].times.user - startCpuUsage[cpu].times.user) / 10,
+        };
+
+        cpuUsage.userPerc = Math.trunc((100 * cpuUsage.user) / usedTime);
+
+        CPUsUsage.push(cpuUsage);
+    }
+
+    let totalCpu = CPUsUsage.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.userPerc,
+        0,
+    );
+
+    let avgCpu = totalCpu / CPUsUsage.length;
+
+    process.stdout.write(` - ${usedTime.toLocaleString()} milliseconds - CPU ${avgCpu}%\n\n`);
+
+    // for (const cpuUsage of CPUsUsage) {
+    //     // console.log(`${usedTime} + ${cpuUsage.user} ${endCpuUsage[cpuUsage.index].times.user} - ${startCpuUsage[cpuUsage.index].times.user}`);
+    //     console.log(`CPU ${cpuUsage.index} usage ${Math.trunc(cpuUsage.userPerc)}%`);
+    // }
+}
